0b97aae2a74f7af521ac5620545d0f730c6eae08
joonghyun <80939285+joong2043@users.noreply.github.com>
2022-10-16 02:24:22 +0900

    feat : 메인페이지 캘린더 생성

--- src/main/java/com/example/concalendar/calendar/controller/CalendarController.java
+++ src/main/java/com/example/concalendar/calendar/controller/CalendarController.java
@@ -1,12 +1,13 @@
 package com.example.concalendar.calendar.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class CalendarController {
     @GetMapping("/")
     public String calendar(){
-        return "calendar";
+        return "main";
     }
 }
