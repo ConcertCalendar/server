9da0531d87fcf66aa7389307de1386f0c480df1a
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-02-09 22:56:45 +0900

    :sparkles: Feat(User) : 로그인 예외처리 구현 및 StatusEnum 클래스 리팩토링

--- src/main/java/com/example/concalendar/util/StatusEnum.java
+++ src/main/java/com/example/concalendar/util/StatusEnum.java
@@ -1,18 +1,24 @@
 package com.example.concalendar.util;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 // Status 상태를 나타내기 쉽게 만든 열거형 데이터
 public enum StatusEnum {
 
-    OK(200,"OK"),
-    BAD_REQUEST(400,"BAD_REQUEST"),
-    NOT_FOUND(404,"NOT_FOUND"),
-    INTERNAL_SERVER_ERROR(500,"INTERNAL_SERVER_ERROR");
+    OK(200, HttpStatus.OK),
+    BAD_REQUEST(400,HttpStatus.BAD_REQUEST),
+    Unauthorized(401,HttpStatus.UNAUTHORIZED),
+    NOT_FOUND(404,HttpStatus.NOT_FOUND),
+    INTERNAL_SERVER_ERROR(500,HttpStatus.INTERNAL_SERVER_ERROR);
 
     int statusCode;
-    String code;
+    HttpStatus httpStatus;
 
-    StatusEnum(int statusCode, String code){
+    StatusEnum(int statusCode,HttpStatus httpStatus){
         this.statusCode = statusCode;
-        this.code = code;
+        this.httpStatus = httpStatus;
+
     }
 }
