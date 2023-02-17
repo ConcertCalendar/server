7ef63b00e0328a49776ab395187288207e46463c
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-02-17 17:17:07 +0900

    :white_check_mark: Test(Post) : 게시글 수정 서비스 테스트 구현

--- src/test/java/com/example/concalendar/post/PostServiceTest.java
+++ src/test/java/com/example/concalendar/post/PostServiceTest.java
@@ -22,14 +22,27 @@ public class PostServiceTest {
     @DisplayName("글 등록하기")
     public void createPostTest(){
         PostFormDto postFormDto = PostFormDto.builder()
-                .postTitle("title1")
-                .postContent("content1")
+                .postTitle("title3")
+                .postContent("content3")
                 .build();
 
         postService.create(postFormDto, "15@gmail.com");
 
         Post post = postService.findPostByPostId(2);
 
-        assertEquals("title1",post.getPostTitle());
+        assertEquals("title3",post.getPostTitle());
+    }
+
+    @Test
+    @DisplayName("글 수정하기")
+    public void updatePostTEst(){
+        PostFormDto postFormDto = PostFormDto.builder()
+                .postTitle("title2 Changed")
+                .postContent("content2 changed")
+                .build();
+
+        Post post = postService.update(2,postFormDto);
+
+        assertEquals("title2 Changed",post.getPostTitle());
     }
 }
