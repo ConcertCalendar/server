e1f96d86da7ee090360f31db4591f4e183fb87c1
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-02-21 21:21:04 +0900

    GEQZ-55 :art: Refactor(Board) : Restful API 설계를 위해 url 리소스(게시판)를 복수형으로 변경 (board -> boards)

--- src/main/java/com/example/concalendar/board/controller/BoardController.java
+++ src/main/java/com/example/concalendar/board/controller/BoardController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/board")
+@RequestMapping("/boards")
 public class BoardController {
     private final PostService postService;
 
