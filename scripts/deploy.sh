d25ebdebc84bfceb02ef8777d7ebce91bdd4356d
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-03-14 01:07:32 +0900

    :bug: Fix(Master) : docker 실행 버그를 nginx restart 명령어로 해결해보기

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,4 +20,5 @@ fi
 
 cd /home/ubuntu/app
 sudo  docker build -t joonghyun/concert_calendar --platform linux/amd64 .
-sudo docker run -d -p 8080:8080 joonghyun/concert_calendar #host에서 사용하는 포트 80, 컨테이너에서 사용하는 포트 8080
+sudo systemctl restart nginx.service
+sudo docker run -p 8080:8080 joonghyun/concert_calendar #host에서 사용하는 포트 80, 컨테이너에서 사용하는 포트 8080
