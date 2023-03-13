d48097f2f5c7530c81fcb1945c069b5c06befa16
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-03-13 22:14:36 +0900

    :memo: Document(Master) : docker 실행을 위해 deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/app/build/libs/*.jar)
-JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/app/deploy.log
+#BUILD_JAR=$(ls /home/ubuntu/app/build/libs/*.jar)
+#JAR_NAME=$(basename $BUILD_JAR)
+#echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/app/deploy.log
+#
+#echo "> build 파일 복사" >> /home/ubuntu/app/deploy.log
+#DEPLOY_PATH=/home/ubuntu/app/
+#cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> build 파일 복사" >> /home/ubuntu/app/deploy.log
-DEPLOY_PATH=/home/ubuntu/app/
-cp $BUILD_JAR $DEPLOY_PATH
-
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/app/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+echo "> 현재 실행중인 Docker 컨테이너 pid 확인" >> /home/ubuntu/app/deploy.log
+CURRENT_PID=$(sudo docker container ls -q)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/app/deploy.log
+  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ubuntu/app/deploy.log
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  echo "> sudo docker stop $CURRENT_PID" # 현재 구동중인 Docker 컨테이너 있으면 모두 중지
   sleep 5
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/app/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/app/deploy_err.log &
+cd /home/ubuntu/app
+sudo  docker build -t joonghyun/concert_calendar --platform linux/amd64 .
+sudo docker run -d -p 8080:8080 joonghyun/concert_calendar
