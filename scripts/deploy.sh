aeeba26902376a68a7cb0b7743172f5150eea225
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-03-14 16:05:30 +0900

    :bug: Fix(Master) : deploy.sh에  추가로 서버 시작 시간 확인 가능

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,14 +7,14 @@
 #DEPLOY_PATH=/home/ubuntu/app/
 #cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 Docker 컨테이너 pid 확인" >> /home/ubuntu/app/deploy.log
+echo "$TIME_NOW > 현재 실행중인 Docker 컨테이너 pid 확인" >> /home/ubuntu/app/deploy.log
 CURRENT_PID=$(sudo docker container ls -q)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ubuntu/app/deploy.log
+  echo "$TIME_NOW > 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ubuntu/app/deploy.log
 else
-  echo "> sudo docker stop $CURRENT_PID" # 현재 구동중인 Docker 컨테이너 있으면 모두 중지
+  echo "$TIME_NOW > sudo docker stop $CURRENT_PID" # 현재 구동중인 Docker 컨테이너 있으면 모두 중지
   sleep 5
 fi
 
@@ -22,4 +22,4 @@ cd /home/ubuntu/app
 sudo  docker build -t joonghyun/concert_calendar --platform linux/amd64 .
 sudo systemctl restart nginx.service
 #sudo docker run -p 8080:8080 joonghyun/concert_calendar #host에서 사용하는 포트 80, 컨테이너에서 사용하는 포트 8080
-sudo docker-compose up
+#sudo docker-compose up
