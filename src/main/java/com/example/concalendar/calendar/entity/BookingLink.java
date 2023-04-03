ec3ee596a6ad59a0dfb63cca1c4c9892fa07f68c
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-04-03 18:17:34 +0900

    GEQZ-120 :art: Refactor(Calendar) : 예매 링크 클래스에 @Getter 추가

--- src/main/java/com/example/concalendar/calendar/entity/BookingLink.java
+++ src/main/java/com/example/concalendar/calendar/entity/BookingLink.java
@@ -1,8 +1,11 @@
 package com.example.concalendar.calendar.entity;
 
+import lombok.Getter;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Getter
 public class BookingLink {
 
     private String yes24Link;
