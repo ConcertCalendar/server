f52944bef6163f8cd344bde3d081a766e0e23b2e
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-06-10 16:10:06 +0900

    GEQZ-174 :sparkles: Feat(Calendar) : 공연 검색 시 띄어쓰기 없는 검색어도 결과가 리턴되도록 Querydsl 수정

--- src/main/java/com/example/concalendar/calendar/repository/CalendarRepositoryCustom.java
+++ src/main/java/com/example/concalendar/calendar/repository/CalendarRepositoryCustom.java
@@ -3,7 +3,10 @@ package com.example.concalendar.calendar.repository;
 import com.example.concalendar.calendar.entity.Calendar;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface CalendarRepositoryCustom {
     public Calendar findNextCalendarByConStart(LocalDate nowDate);
+    List<Calendar> findCalendarsByConTitleContains(String searchKeyword);
+
 }
