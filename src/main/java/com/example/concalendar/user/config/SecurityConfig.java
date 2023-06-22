6c4f9feda9e3e7e09945b3df4730616a817017ff
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-06-22 18:42:12 +0900

    :art: Refactor(User) : 불필요한 주석 삭제

--- src/main/java/com/example/concalendar/user/config/SecurityConfig.java
+++ src/main/java/com/example/concalendar/user/config/SecurityConfig.java
@@ -5,15 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
@@ -42,14 +39,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder;
     }
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().
-//                    withUser("user").password("{noop}password1").roles("USER")
-//                .and()
-//                    .withUser("admin").password("{noop}password2").roles("ADMIN");
-//    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
--- src/main/java/com/example/concalendar/user/dto/UserInfoDto.java
+++ src/main/java/com/example/concalendar/user/dto/UserInfoDto.java
@@ -1,17 +1,8 @@
 package com.example.concalendar.user.dto;
 
-import com.example.concalendar.comment.entity.Comment;
-import com.example.concalendar.post.entity.Post;
-import com.example.concalendar.reply.entity.Reply;
 import com.example.concalendar.user.entity.User;
-import lombok.Builder;
 import lombok.Getter;
 
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -36,9 +27,6 @@ public class UserInfoDto {
         this.userNickname = user.getUserNickname();
         this.userBirth = user.getUserBirth();
         this.name = user.getName();
-//        this.postList = user.getPostList();
-//        this.commentList = user.getCommentList();
-//        this.replyList = user.getReplyList();
         this.roles = user.getRoles();
         this.userProfileImgUrl = user.getProfileImageUrl();
     }
